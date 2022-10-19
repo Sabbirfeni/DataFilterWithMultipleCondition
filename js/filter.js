ec8ce220ef5f88806435bad80cd6dbd5bf027c1e
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2022-10-19 14:27:31 +0600

    Auto player search implemented and google sheet has benn connected successfully

--- js/filter.js
+++ js/filter.js
@@ -6,7 +6,7 @@
   {
     {
       // Your API KEY
-      const API_KEY = "";
+      const API_KEY = "AIzaSyCa5GgOTng3c0t_CHT_XOASd3JFJN0UGfc";
 
       function displayResult2(response) {
         let tableHead = "";
@@ -28,20 +28,160 @@
         document.getElementById("table-body").innerHTML = tableBody;
      
 
-        const allTableData = document.querySelectorAll("#emp-table > tbody > tr > td")
+        const allTableData = document.querySelectorAll("#emp-table > tbody > tr > td:last-child");
 
-        allTableData.forEach((singleTableData) => {
-          singleTableData.setAttribute('id', 'table_data');
-        })
+        allTableData.forEach((item, index, arr) => {
 
-
-        const allTableRow = document.querySelectorAll("#emp-table > tbody > tr > td:nth-last-child(odd)");
-        const lastChild = document.querySelectorAll("#emp-table > tbody > tr > td:nth-last-child");
-        allTableRow.forEach((value, index, arr) => {
-
-          console.log(lastChild)
+        item.setAttribute('class', 'player')
+        
           
         })
+
+
+
+
+
+
+
+    // Auto search of player names
+    // ----------------------------
+    function autocomplete(inp, arr) {
+      var currentFocus;
+      
+      inp.addEventListener("input", function(e) {
+          var a, b, i, val = this.value;
+          
+          let uniquePlayer = [...new Set(arr)]
+
+
+          closeAllLists();
+          if (!val) { return false;};
+          currentFocus = -1;
+          
+          a = document.createElement("DIV");
+          a.setAttribute("id", this.id + "autocomplete-list");
+          a.setAttribute("class", "autocomplete-items");
+          
+          this.parentNode.appendChild(a);
+          /*for each item in the array...*/
+
+
+          for (i = 0; i < uniquePlayer.length; i++) {
+
+              
+            if (uniquePlayer[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+              /*create a DIV element for each matching element:*/
+              b = document.createElement("DIV");
+              // b.classList.add('player__name-onWriting');
+              /*make the matching letters bold:*/
+              b.innerHTML = "<strong>" + uniquePlayer[i].substr(0, val.length) + "</strong>";
+              b.innerHTML += uniquePlayer[i].substr(val.length);
+              
+              b.innerHTML += "<input type='hidden' value='" + uniquePlayer[i] + "'>";
+              
+              b.addEventListener("click", function(e) {
+                  /*insert the value for the autocomplete text field:*/
+                  inp.value = this.getElementsByTagName("input")[0].value;
+                  closeAllLists();
+                  filter_rows()
+              });
+              a.appendChild(b);
+            }
+          }
+      });
+      
+      inp.addEventListener("keydown", function(e) {
+          var x = document.getElementById(this.id + "autocomplete-list");
+          if (x) x = x.getElementsByTagName("div");
+          if (e.keyCode == 40) {
+              
+            currentFocus++;
+            
+            addActive(x);
+          } else if (e.keyCode == 38) { 
+
+            currentFocus--;
+            
+            addActive(x);
+          } else if (e.keyCode == 13) {
+              
+            e.preventDefault();
+            if (currentFocus > -1) {
+              
+              if (x) x[currentFocus].click();
+            }
+          }
+      });
+      function addActive(x) {
+          
+        if (!x) return false;
+        
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        
+        x[currentFocus].classList.add("autocomplete-active");
+      }
+      function removeActive(x) {
+          
+        for (var i = 0; i < x.length; i++) {
+          x[i].classList.remove("autocomplete-active");
+        }
+      }
+      function closeAllLists(elmnt) {
+
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+          if (elmnt != x[i] && elmnt != inp) {
+            x[i].parentNode.removeChild(x[i]);
+          }
+        }
+      }
+      
+      document.addEventListener("click", function (e) {
+          closeAllLists(e.target);
+      });
+    }
+
+
+    // var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+
+    var allPlayer = document.querySelectorAll('.player');
+    let allPlayerName = [];
+
+    allPlayer.forEach((valueName, index) => {
+      let playerName = valueName.innerText;
+      allPlayerName.push(playerName);
+    })
+
+    autocomplete(document.getElementById("myInput"), allPlayerName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        // allTableData.forEach((singleTableData) => {
+        //   singleTableData.setAttribute('id', 'table_data');
+        // })
+
+
+        // const allTableRow = document.querySelectorAll("#emp-table > tbody > tr > td:nth-last-child(odd)");
+        // const lastChild = document.querySelectorAll("#emp-table > tbody > tr > td:nth-last-child");
+        // allTableRow.forEach((value, index, arr) => {
+
+        //   console.log(lastChild)
+          
+        // })
 
 
 
@@ -196,118 +336,4 @@ function filter_rows() {
 
 
 
-
-// Auto search of player names
-// ----------------------------
-
-function autocomplete(inp, arr) {
-  var currentFocus;
-  
-  inp.addEventListener("input", function(e) {
-      var a, b, i, val = this.value;
-      
-      let uniquePlayer = [...new Set(arr)]
-
-
-      closeAllLists();
-      if (!val) { return false;};
-      currentFocus = -1;
-      
-      a = document.createElement("DIV");
-      a.setAttribute("id", this.id + "autocomplete-list");
-      a.setAttribute("class", "autocomplete-items");
-      
-      this.parentNode.appendChild(a);
-      /*for each item in the array...*/
-
-
-      for (i = 0; i < uniquePlayer.length; i++) {
-
-          
-        if (uniquePlayer[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-          /*create a DIV element for each matching element:*/
-          b = document.createElement("DIV");
-          // b.classList.add('player__name-onWriting');
-          /*make the matching letters bold:*/
-          b.innerHTML = "<strong>" + uniquePlayer[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += uniquePlayer[i].substr(val.length);
-          
-          b.innerHTML += "<input type='hidden' value='" + uniquePlayer[i] + "'>";
-          
-          b.addEventListener("click", function(e) {
-              /*insert the value for the autocomplete text field:*/
-              inp.value = this.getElementsByTagName("input")[0].value;
-              closeAllLists();
-              filter_rows()
-          });
-          a.appendChild(b);
-        }
-      }
-  });
-  
-  inp.addEventListener("keydown", function(e) {
-      var x = document.getElementById(this.id + "autocomplete-list");
-      if (x) x = x.getElementsByTagName("div");
-      if (e.keyCode == 40) {
-          
-        currentFocus++;
-        
-        addActive(x);
-      } else if (e.keyCode == 38) { 
-
-        currentFocus--;
-        
-        addActive(x);
-      } else if (e.keyCode == 13) {
-          
-        e.preventDefault();
-        if (currentFocus > -1) {
-          
-          if (x) x[currentFocus].click();
-        }
-      }
-  });
-  function addActive(x) {
-      
-    if (!x) return false;
-    
-    removeActive(x);
-    if (currentFocus >= x.length) currentFocus = 0;
-    if (currentFocus < 0) currentFocus = (x.length - 1);
-    
-    x[currentFocus].classList.add("autocomplete-active");
-  }
-  function removeActive(x) {
-      
-    for (var i = 0; i < x.length; i++) {
-      x[i].classList.remove("autocomplete-active");
-    }
-  }
-  function closeAllLists(elmnt) {
-
-    var x = document.getElementsByClassName("autocomplete-items");
-    for (var i = 0; i < x.length; i++) {
-      if (elmnt != x[i] && elmnt != inp) {
-        x[i].parentNode.removeChild(x[i]);
-      }
-    }
-  }
-  
-  document.addEventListener("click", function (e) {
-      closeAllLists(e.target);
-  });
-}
-
-
-var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-
-var allPlayer = document.querySelectorAll('.player');
-let allPlayerName = [];
-
-allPlayer.forEach((valueName, index) => {
-  let playerName = valueName.innerText;
-  allPlayerName.push(playerName);
-})
-
-autocomplete(document.getElementById("myInput"), allPlayerName);
 
