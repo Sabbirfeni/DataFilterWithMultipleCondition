1b6830663cb4ea27f04f97edb785244e640f2e32
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2022-10-29 21:13:43 +0600

    serach bar and dynamic filter bar add

--- js/filter.js
+++ js/filter.js
@@ -189,6 +189,52 @@ function getUniqueValuesFromColumn() {
 
 
 
+
+  // Filter data using search bar
+  //-----------------------------
+    function search_animal() {
+      let input = document.getElementById('searchbar').value
+      input=input.toLowerCase();
+      let x1 = document.querySelectorAll('#emp-table > tbody > tr');
+
+      x1.forEach((value) => {
+
+        const tdArray = value.getElementsByTagName('td');
+
+        for(let i = 0; i < tdArray.length; i++) {
+
+          if(tdArray[i].innerHTML.toLowerCase().includes(input)) {
+            value.style.display = 'block';
+            return 
+          } else {
+            value.style.display = 'none';
+          }
+
+        }
+      })
+
+
+      // Test code for search bar (Can be removed)
+      // -----------------------------------------
+
+      
+      // let x = document.getElementsByClassName('animals');
+      // for (i = 0; i < x.length; i++) { 
+      //     if (!x[i].innerHTML.toLowerCase().includes(input)) {      
+      //         x[i].style.display="none";
+      //     }
+      //     else {
+      //         x[i].style.display="list-item";                 
+      //     }
+      // }
+    }
+
+
+
+
+
+
+
 // Create filter_rows() function
 // filter_value_dict {2 : Value selected, 4:value, 5: value}
 // ---------------------------------------------------------
